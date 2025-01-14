a0caaf5ace8865024d3351b877d2462ca688f9e3
kunal <kunalraj72@gmail.com>
2025-01-14 22:31:26 +0530

    added store page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ProductPage from './pages/ProductPage';
-import ModelViewer from './pages/components/ModelViewer';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ModelViewer from "./pages/components/ModelViewer";
+import "./App.css";
+import StorePage from "./pages/StorePage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/store" element={<ProductPage />} />
-        <Route path="/contact" element={<div className="page">Contact Page</div>} />
-        <Route path="/lookbook" element={<div className="page">Lookbook Page</div>} />
-        <Route path="/return-policy" element={<div className="page">Return Policy Page</div>} />
-        <Route path="/pre-order-status" element={<div className="page">Pre-order Status Page</div>} />
-        <Route path="/3d-view" element={<ModelViewer modelPath="/models/ring.obj" />} />
+        <Route path="/store" element={<StorePage />} />
+        <Route
+          path="/contact"
+          element={<div className="page">Contact Page</div>}
+        />
+        <Route
+          path="/lookbook"
+          element={<div className="page">Lookbook Page</div>}
+        />
+        <Route
+          path="/return-policy"
+          element={<div className="page">Return Policy Page</div>}
+        />
+        <Route
+          path="/pre-order-status"
+          element={<div className="page">Pre-order Status Page</div>}
+        />
+        <Route
+          path="/3d-view"
+          element={<ModelViewer modelPath="/models/ring.obj" />}
+        />
       </Routes>
     </Router>
   );
---
+++ src/pages/StorePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProductPage from './pages/ProductPage';
+
+
+export default function StorePage() {
+  return (
+      <div >
+          
+    </div>
+  )
+}
